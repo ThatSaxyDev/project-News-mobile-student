b19f34a1aaa3b7544dfbb63e76dfcbd6aa3556d9
Kiishi David <user@USERs-MacBook-Pro.local>
2022-09-03 00:22:24 +0100

    changed alert function name

--- lib/shared/utils/alert.dart
+++ lib/shared/utils/alert.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cherry_toast/cherry_toast.dart';
 
-void showSnackBar(BuildContext context, String text) {
+void showAlert(BuildContext context, String text) {
   CherryToast.info(
           title: Text(text),
           animationDuration: const Duration(milliseconds: 1000),
